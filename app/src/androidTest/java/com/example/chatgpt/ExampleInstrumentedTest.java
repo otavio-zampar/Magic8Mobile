075d70f5c529b4a17a582213220d9f5f4145aaa0
Lucas Ribeiro <70024244+Lucas2003-18@users.noreply.github.com>
2023-05-10 07:59:00 -0300

    Add files via upload

--- app/src/androidTest/java/com/example/chatgpt/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/chatgpt/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.chatgpt;
+package com.example.chatgptformobile;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.chatgpt", appContext.getPackageName());
+        assertEquals("com.example.chatgptformobile", appContext.getPackageName());
     }
 }
